e0d8e1e7438df91c2cc0245ab59d29fdb3167f18
pacokwon <haechank@gmail.com>
2020-11-09 03:15:03 +0000

    update: apply indentations and styles to mysplit

--- mysplit.c
+++ mysplit.c
@@ -1,6 +1,6 @@
-/* 
+/*
  * mysplit.c - Another handy routine for testing your tiny shell
- * 
+ *
  * usage: mysplit <n>
  * Fork a child that spins for <n> seconds in 1-second chunks.
  */
@@ -11,21 +11,20 @@
 #include <sys/wait.h>
 #include <signal.h>
 
-int main(int argc, char **argv) 
+int main(int argc, char **argv)
 {
     int i, secs;
 
     if (argc != 2) {
-	fprintf(stderr, "Usage: %s <n>\n", argv[0]);
-	exit(0);
+        fprintf(stderr, "Usage: %s <n>\n", argv[0]);
+        exit(0);
     }
     secs = atoi(argv[1]);
 
-
     if (fork() == 0) { /* child */
-	for (i=0; i < secs; i++)
-	    sleep(1);
-	exit(0);
+        for (i = 0; i < secs; i++)
+            sleep(1);
+        exit(0);
     }
 
     /* parent waits for child to terminate */
